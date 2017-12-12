853517c110d051f623ac1253d15f411f606dbf7a
SimonBiggs <mail@simonbiggs.net>
2017-12-12 21:48:30 +1100

    wording

--- src/angular-app/sections-module/button.component.ts
+++ src/angular-app/sections-module/button.component.ts
@@ -2,7 +2,7 @@
 // Copyright (C) 2017 Simon Biggs
 
 // Licensed under both the Apache License, Version 2.0 (the "Apache-2.0") and 
-// GNU Affrero General Public License as published by the Free Software 
+// the GNU Affrero General Public License as published by the Free Software 
 // Foundation, either version 3 of the License, or (at your option) any later 
 // version (the "AGPL-3.0+").
 
@@ -18,10 +18,10 @@
 //     https://www.apache.org/licenses/LICENSE-2.0.html
 
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the Apache-2.0 and the AGPL-3.0+ is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the Apache-2.0 and the AGPL-3.0+ for the specific language governing 
-// permissions and limitations under the License.
+// permissions and limitations under the Apache-2.0 and the AGPL-3.0+.
 
 /*
 Creates the [button] section.
